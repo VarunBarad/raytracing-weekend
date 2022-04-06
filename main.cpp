a3ee9f1a7427d93e46dd1c006529ea56df351eb2
Varun Barad <varun@varunbarad.com>
2022-04-06 23:04:47 +0530

    Add progress indicator

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
     std::cout << "P3" << std::endl << image_width << ' ' << image_height << std::endl << "255" << std::endl;
 
     for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_width - 1);
@@ -22,5 +23,6 @@ int main() {
         }
     }
 
+    std::cerr << std::endl << "Done." << std::endl;
     return 0;
 }
