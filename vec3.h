1cce3087356f12f8603dd8f7f87330d76d1f2f8b
Varun Barad <varun@varunbarad.com>
2022-04-10 11:16:45 +0530

    Fix errors in logic

--- vec3.h
+++ vec3.h
@@ -26,23 +26,23 @@ public:
 
     double operator[](int i) const { return e[i]; }
 
-    double &operator[](int i) { return e[i]; }
+    double& operator[](int i) { return e[i]; }
 
-    vec3 &operator+=(const vec3 &v) {
+    vec3& operator+=(const vec3 &v) {
         e[0] += v.e[0];
         e[1] += v.e[1];
         e[2] += v.e[2];
         return *this;
     }
 
-    vec3 &operator*=(const double t) {
+    vec3& operator*=(const double t) {
         e[0] *= t;
         e[1] *= t;
         e[2] *= t;
         return *this;
     }
 
-    vec3 &operator/=(const double t) {
+    vec3& operator/=(const double t) {
         return *this *= 1 / t;
     }
 
@@ -64,7 +64,7 @@ using color = vec3; // RGB color
 
 // vec3 utility functions
 
-inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
@@ -88,14 +88,14 @@ inline vec3 operator*(const vec3 &v, double t) {
     return t * v;
 }
 
-inline vec3 operator/(const vec3 &v, double t) {
+inline vec3 operator/(vec3 v, double t) {
     return (1 / t) * v;
 }
 
 inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0]
-           + u.e[0] * v.e[0]
-           + u.e[0] * v.e[0];
+           + u.e[1] * v.e[1]
+           + u.e[2] * v.e[2];
 }
 
 inline vec3 cross(const vec3 &u, const vec3 &v) {
